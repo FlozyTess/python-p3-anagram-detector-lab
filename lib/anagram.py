e7ee0e9f19744daa883cf78f9cea481fb1b808b8
FlozyTess <cecilrency23@gmail.com>
2025-03-09 12:08:19 +0300

    created anagram detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()  # Store the word in lowercase for case insensitivity
+
+    def match(self, words):
+        return [w for w in words if sorted(w.lower()) == sorted(self.word) and w.lower() != self.word]
